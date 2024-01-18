d9e86fd245e80cc6a87b3c8899e1b9fdff766529
unknown <areyprada@gmail.com>
2024-01-17 23:01:11 -0500

    se inicia apartado de buscador principal

--- src/componentes/BuscarProducto.jsx
+++ src/componentes/BuscarProducto.jsx
@@ -1,10 +1,43 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import '../css/header.css'
+import '../css/buscarProducto.css'
+
 import buscarIco from '../../public/lupa.png'
+import flechaVoler from '../../public/flecha.png'
 
 const BuscarProducto = () => {
+
+  const [isBuscadorDesplegado, setIsMenuDesplegado] = useState(false);
+
+  const toggleBuscador = () => {
+    setIsMenuDesplegado(!isBuscadorDesplegado)
+  }
+
+
   return (
     <div>
+      <div className='iconoHeaderAbrir' onClick={toggleBuscador}>
         <img className='header-img-iconos' src={buscarIco} alt="" />
+      </div>
+      {isBuscadorDesplegado && (
+        <div className="popup-bg-buscador">
+          <div className="popup-buscador">
+            <div className='divBuscador'>
+              <div className='divImgVolver'>
+                <img onClick={toggleBuscador} className="close-flecha-img" src={flechaVoler} alt="Cerrar" />
+              </div>
+              <input
+                type="text"
+                className='inputBuscador'
+                placeholder='Buscar...'
+              />
+            </div>
+
+
+
+          </div>
+        </div>
+      )}
     </div>
   )
 }
